44a3ccced14aba4261c4c68ba9afa6a7cfe7978f
Jayke770 <jakevelasco40@gmail.com>
2023-08-13 08:39:50 +0000

    .

--- components/Client/Onboarding/index.tsx
+++ components/Client/Onboarding/index.tsx
@@ -1,4 +1,4 @@
-import { memo, useEffect } from 'react'
+import { memo, useCallback, useEffect } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 import 'swiper/css/pagination'
@@ -7,8 +7,16 @@ import Image from 'next/image'
 import IntroImage from '@/public/images/onboarding/intro.png'
 import CupsImage from '@/public/images/onboarding/cups.png'
 import { Button } from 'konsta/react'
-import { motion } from 'framer-motion'
+import { motion, Variants } from 'framer-motion'
+import { useLocalstorageState } from 'rooks'
+const variants: Variants = {
+    initial: {
+        
+    }
+}
 const Onboarding = () => {
+    const [isNew, setisNew] = useLocalstorageState<boolean>("isNew", false)
+    const onToggleIsNew = useCallback(() => setisNew(e => !e), [setisNew])
     return (
         <motion.div
             initial={{ opacity: 0, scale: 1.1 }}
@@ -16,7 +24,7 @@ const Onboarding = () => {
             exit={{ opacity: 0, scale: 1.1 }}
             transition={{ ease: "easeInOut", duration: 0.5, delay: 0.2 }}
             id='onboarding'
-            className='fixed h-full w-full'>
+            className='fixed h-full w-full p-2 bg-coffee-primary '>
             <Swiper
                 pagination={{
                     dynamicBullets: true,
@@ -27,18 +35,19 @@ const Onboarding = () => {
                 }}
                 grabCursor={true}
                 modules={[Pagination, Navigation]}
-                className=' bg-coffee-primary h-full w-full'>
+                className='h-full w-full'>
                 <SwiperSlide>
                     <div className='flex flex-col gap-4 w-full h-full justify-center items-center'>
                         <Image
                             src={IntroImage}
                             alt='intro'
+                            priority
                             className=' w-80 h-auto' />
                         <div className='mt-5 flex flex-col gap-8'>
                             <h1 className='text-coffee-secondary font-bold text-3xl px-8 text-center'>Coffee so good, your taste buds will love it</h1>
                             <p className='font-medium px-8 text-center text-lg'>The best grain, the finest roast, the most powerful flavor.</p>
                         </div>
-                        <div className='flex w-full md:w-[20%] mt-8 px-8'>
+                        <div className='flex w-full md:w-[20%] mt-10 px-8'>
                             <Button
                                 id='next'
                                 large
@@ -51,14 +60,16 @@ const Onboarding = () => {
                     <div className='flex flex-col gap-4 w-full h-full justify-center items-center'>
                         <Image
                             src={CupsImage}
-                            alt='intro'
+                            alt='cups'
+                            priority
                             className=' w-80 h-auto' />
-                        <div className='mt-5 flex flex-col gap-2'>
+                        <div className='mt-5 flex flex-col gap-8'>
                             <h1 className='text-coffee-secondary font-bold text-3xl px-8 text-center'>Discover our signature espresso</h1>
                             <p className='font-medium px-8 text-center text-lg'>{"We've compiled a wide selection of blends and beans to fill your cup"}</p>
                         </div>
-                        <div className='flex w-full md:w-[20%] mt-3 px-8'>
+                        <div className='flex w-full md:w-[20%] mt-10 px-8'>
                             <Button
+                                onClick={onToggleIsNew}
                                 large
                                 rounded
                                 className='w-full k-color-brand-green uppercase'>{"Let's Coffee"}</Button>
