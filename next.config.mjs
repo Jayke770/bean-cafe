adf9c6ee755fe0ec0b56610a91863beeadf36a7f
Jayke770 <jakevelasco40@gmail.com>
2023-08-13 01:32:50 +0000

    .

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+import million from 'million/compiler';
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+const millionConfig = {
+  auto: true,
+}
+export default million.next(nextConfig, millionConfig);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { App } from "konsta/react"
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
+export default function BeanCafe({ Component, pageProps }: AppProps) {
+  return (
+    <App
+      className={inter.className}
+      safeAreas
+      dark>
+      <Component {...pageProps} />
+    </App>
+  )
 }
