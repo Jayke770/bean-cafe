f305e59580be727916648673fcd92a9cc13daf33
Jayke770 <jakevelasco40@gmail.com>
2023-08-20 20:34:03 +0800

    .

--- app/admin/dashboard/navbar.tsx
+++ app/admin/dashboard/navbar.tsx
@@ -1,12 +1,19 @@
 "use client"
 import { Navbar, Link, Icon } from 'konsta/react'
-import { HiOutlineBell } from 'react-icons/hi2'
+import { HiBars3BottomRight, HiOutlineBell } from 'react-icons/hi2'
 export default function NavbarAdmin() {
     return (
         <Navbar
             titleClassName='!font-bold'
             title='Dashboard'
             className=' k-color-brand-primary'
+            left={
+                <Link navbar iconOnly className=' lg:hidden'>
+                    <Icon>
+                        <HiBars3BottomRight className='h-6 w-6' />
+                    </Icon>
+                </Link>
+            }
             right={
                 <>
                     <Link navbar iconOnly>
--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,11 +1,31 @@
-import { Card } from "konsta/react";
+"use client"
 import NavbarAdmin from "./navbar";
 import Cards from "./cards";
+import { motion, AnimatePresence, Variants } from 'framer-motion'
+const variants: Variants = {
+    initial: {
+        opacity: 0,
+        x: -50
+    },
+    animate: {
+        opacity: 1,
+        x: 1
+    },
+    exit: {
+        opacity: 0,
+        x: -50
+    }
+}
 export default function DashBoard() {
     return (
-        <>
-            <NavbarAdmin />
-            <Cards />
-        </>
+        <AnimatePresence mode="wait">
+            <nav className=" fixed hidden lg:block w-18 bg-md-light-surface-2 dark:bg-md-dark-surface-2 k-color-brand-primary h-full">
+
+            </nav>
+            <div className="lg:ml-18 transition-all">
+                <NavbarAdmin />
+                <Cards />
+            </div>
+        </AnimatePresence>
     )
 }
