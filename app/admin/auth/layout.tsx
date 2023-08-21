f326544c6e0cfec5535306a9c380c6304d8304ea
Jayke770 <jakevelasco40@gmail.com>
2023-08-21 14:10:09 +0800

    .

--- app/admin/auth/layout.tsx
+++ app/admin/auth/layout.tsx
@@ -3,7 +3,7 @@ export const metadata: Metadata = {
     title: "Bean Cafe - Admin Authentication",
     description: 'Bean Cafe'
 }
-export default async function AuthLayout({ children }: { children: React.ReactNode }) {
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
     return (
         <main className='h-full z-10 overflow-hidden flex justify-center items-center w-full left-0 top-0 fixed bg-brand-white dark:bg-brand-secondary/20' >
             {children}
--- app/admin/auth/page.tsx
+++ app/admin/auth/page.tsx
@@ -1,13 +1,90 @@
-import Card from './card'
-import { getServerSession } from 'next-auth'
-import { AuthOptions } from '@services/NextAuth/AuthOptions'
-import { redirect } from 'next/navigation'
-export default async function AdminAuth() {
-    const session = await getServerSession(AuthOptions)
-    if (session?.user?.role === "admin") redirect("/admin/dashboard")
+"use client"
+import { Button, Card, Preloader } from "konsta/react"
+import { signIn, useSession } from "next-auth/react"
+import { FcGoogle } from "react-icons/fc"
+import { AnimatePresence, motion, type Variants } from 'framer-motion'
+import { useEffect } from "react"
+import { useRouter } from "next/navigation"
+const variants: Variants = {
+    initial: {
+        opacity: 0,
+        scale: 1.1
+    },
+    animate: {
+        opacity: 1,
+        scale: 1
+    },
+    exit: {
+        opacity: 0,
+        scale: 1.1
+    }
+}
+export default function AdminAuth() {
+    const router = useRouter()
+    const { status } = useSession()
+    useEffect(() => {
+        if (status === "authenticated") router.push("/admin/dashboard")
+    }, [status, router])
     return (
         <>
-            <Card />
+            <AnimatePresence mode="wait">
+                {(status === "loading" || status === "authenticated") && (
+                    <Preloader className=" k-color-brand-primary" />
+                )}
+                {status === "unauthenticated" && (
+                    <motion.div
+                        variants={variants}
+                        initial={"initial"}
+                        animate={"animate"}
+                        exit={"exit"}
+                        transition={{ ease: "easeInOut", duration: 0.3 }}
+                        className="w-full md:w-[400px] m-4">
+                        <Card
+                            raised
+                            margin="m-0"
+                            className=" k-color-brand-primary w-full translucent z-10 ">
+                            <div className="p-4">
+                                <div className="flex flex-col mb-5 gap-1">
+                                    <h1 className="text-3xl font-bold text-brand-primary">Bean Cafe</h1>
+                                    <span className="">Hi, Welcome Back 👋</span>
+                                </div>
+                                <Button
+                                    onClick={() => signIn("google", { callbackUrl: "/admin/dashboard" })}
+                                    clear
+                                    outline
+                                    className=' k-color-brand-white'>
+                                    <div className='h-full w-full flex items-center justify-center gap-2'>
+                                        <FcGoogle className=' h-5 w-5' />
+                                        <span className=' text-black dark:text-zinc-200 '>Sign in with Google</span>
+                                    </div>
+                                </Button>
+                                <hr className=' border-1 border-black dark:border-brand-primary my-5' />
+                                <div className="flex flex-col gap-2.5">
+                                    <div className='flex flex-col gap-2'>
+                                        <label htmlFor="email" className="block text-sm font-medium">Email</label>
+                                        <input
+                                            type="email"
+                                            id="email"
+                                            className="py-3 px-4 block w-full dark:bg-transparent dark:border-brand-primary/50 border-brand-secondary/50 border transition-all rounded-md outline-none text-sm focus:border-brand-primary focus:ring-1 focus:ring-brand-primary"
+                                            placeholder="jhon@email.com"
+                                            aria-describedby="email" />
+                                    </div>
+                                    <div className='flex flex-col gap-2'>
+                                        <label htmlFor="password" className="block text-sm font-medium">Password</label>
+                                        <input
+                                            type="password"
+                                            id="password"
+                                            className="py-3 px-4 block w-full dark:bg-transparent dark:border-brand-primary/50 border-brand-secondary/50 border transition-all rounded-md outline-none text-sm focus:border-brand-primary focus:ring-1 focus:ring-brand-primary"
+                                            placeholder="********"
+                                            aria-describedby="password" />
+                                    </div>
+                                    <Button className="mt-1">Sign in</Button>
+                                </div>
+                            </div>
+                        </Card>
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </>
     )
 }
