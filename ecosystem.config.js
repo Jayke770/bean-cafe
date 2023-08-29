21adf770e41d557e219c451d358ff094446c24fd
Jayke770 <jakevelasco40@gmail.com>
2023-08-29 18:21:21 +0800

    enable standalone mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,10 +3,9 @@ module.exports = {
     {
       name: "Bean Cafe",
       cron_restart: "0 0 * * *",
-      script: "node_modules/next/dist/bin/next",
-      args: "start -p 80",
-      // exec_mode: "cluster",
-      // instances: "max",
+      script: ".next/standalone/server.js",
+      exec_mode: "cluster",
+      instances: "max",
     },
   ],
 };
