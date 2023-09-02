6035552167fc6e81f4851665a691f054bc9ed9df
Jayke770 <jakevelasco40@gmail.com>
2023-09-02 22:11:11 +0800

    .

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,2 @@
 export const SIZES = ["Small", "Medium", "Large", "Extra Large"];
-export const CATEGORIES = ["Coffee", "Burger", "Cakes"];
+export const CATEGORIES = ["Coffee", "Milk Tea", "Burger", "Cakes"];
