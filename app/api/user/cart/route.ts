fd819668cdbb933fc06d1982fb3c1bc27255c6f2
Jayke770 <jakevelasco40@gmail.com>
2023-09-13 22:22:19 +0800

    .

--- app/api/user/cart/route.ts
+++ app/api/user/cart/route.ts
@@ -29,12 +29,29 @@ export async function POST(req: NextRequest) {
                     //check if the item is already added to cart 
                     const isAlreadyAdded = await User.findOne({
                         _id: { $eq: session.user.id },
-                        cart: { $elemMatch: { item_id: data.item_id, size: data.selected_size } }
-                    }, { cart: { $elemMatch: { item_id: data.item_id, size: data.selected_size } } })
+                        cart: {
+                            $elemMatch: {
+                                item_id: { $eq: data.item_id },
+                                size: { $eq: data.selected_size }
+                            }
+                        }
+                    }, {
+                        cart: {
+                            $elemMatch: {
+                                item_id: { $eq: data.item_id },
+                                size: { $eq: data.selected_size }
+                            }
+                        }
+                    })
                     if (isAlreadyAdded) {
                         await User.updateOne({
                             _id: { $eq: session.user.id },
-                            cart: { $elemMatch: { item_id: data.item_id, size: data.selected_size } }
+                            cart: {
+                                $elemMatch: {
+                                    item_id: { $eq: data.item_id },
+                                    size: { $eq: data.selected_size }
+                                }
+                            }
                         }, {
                             $inc: { "cart.$.quantity": data.quantity }
                         })
