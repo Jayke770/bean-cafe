07e170850c5fc11721f281a1d40ccbe1a115f8d2
Jayke770 <jakevelasco40@gmail.com>
2023-09-22 18:19:32 +0800

    .

--- app/api/user/items/checkout/route.ts
+++ app/api/user/items/checkout/route.ts
@@ -4,12 +4,15 @@ import { AuthOptions } from '@services/NextAuth/AuthOptions'
 import { z } from 'zod'
 import dbConnect from '@/models/dbConnect';
 import { ApiResponse } from '@/types';
+import Email from '@/services/email';
+const emailHandler = new Email("Bean Cafe")
 export async function POST(req: NextRequest) {
     const session = await getServerSession(AuthOptions)
     let res: ApiResponse = {}
     try {
         if (session) {
             await dbConnect()
+            if (session?.user?.email) emailHandler.send({ receiver: session.user.email, subject: "Check out", body: "test checkout" })
             res = {
                 status: false,
                 message: "Under Maintenance!"
