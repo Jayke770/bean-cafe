3b3a5b629f6522ced2d80ea47e1311689a4539f9
jayke770 <jakevelasco40@gmail.com>
2023-10-03 19:50:19 +0800

    .

--- app/payment/success/page.tsx
+++ app/payment/success/page.tsx
@@ -1,6 +1,6 @@
 export default function Cancelled() {
     return (
-        <div className="flex w-screen h-screen justify-center items-center">
+        <div className="flex  w-screen h-screen justify-center items-center">
             <div className=' left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 z-40 max-h-full overflow-hidden duration-400 fixed rounded-[1.75rem] p-6 max-w-[90%] w-[19.5rem] bg-md-light-surface-3 dark:bg-md-dark-surface-3 scale-[0.85] opacity-0 pointer-events-none  k-color-brand-primary'>
                 <div className="p-4">
                     ffgfsafa
