3d92142e28cf25baf653657fe16c73155197e9e0
jayke770 <jakevelasco40@gmail.com>
2023-10-04 12:37:41 +0800

    .

--- app/payment/success/page.tsx
+++ app/payment/success/page.tsx
@@ -1,14 +1,17 @@
-"use client"
+'use client'
+import { Suspense } from 'react'
 import OrderInfo from '@/lib/User/orderInfo';
 import { Success, SuccessLoading } from '@components/payment_card'
 import { Button } from "konsta/react";
 import { useSearchParams } from 'next/navigation'
 export default function PaytmentSuccess() {
     const searchParams = useSearchParams()
-    const { orderData, orderDataLoading } = OrderInfo(searchParams.get("orderId"))
+    const { orderData } = OrderInfo(searchParams.get("orderId"))
     return (
         <div className="flex fixed top-0 left-0 z-50 w-screen h-screen justify-center items-center bg-brand-white dark:bg-brand-secondary/20">
-            {orderDataLoading ? <SuccessLoading /> : <Success data={orderData} />}
+            <Suspense fallback={<SuccessLoading />}>
+                <Success data={orderData} />
+            </Suspense>
         </div>
     )
 }
