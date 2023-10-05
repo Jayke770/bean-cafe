1f217d6609dba58ad4b923676e0eb00396f654c1
Jayke770 <jakevelasco40@gmail.com>
2023-10-05 13:10:48 +0000

    .

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -1,8 +1,6 @@
-"use client"
 import "rc-steps/assets/index.css"
-import MainStep, { Step } from 'rc-steps'
+import MainStep from 'rc-steps'
 import { BsInfoCircle } from 'react-icons/bs'
-import { Card } from 'konsta/react'
 export default function OrderInfo() {
     return (
         <div className="p-4 h-full z-5 w-full left-0 top-0 overflow-auto absolute bg-brand-white dark:bg-brand-secondary/20">
