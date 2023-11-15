6093d7a132e11b1b68293bb72682cf0fe892f4e0
jayke770 <jakevelasco40@gmail.com>
2023-11-15 10:02:56 +0800

    .

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,15 +1,15 @@
-// import { NextResponse } from 'next/server'
-// import Paypal from '@/services/paypal'
-// import Twillio from '@/services/twilio'
-// const twillio = new Twillio()
-// const { PAYPAL_SECRET, PAYPAL_CLIENT_ID, NEXTAUTH_URL } = process.env
-// const paypal = new Paypal({
-//     client_secret: PAYPAL_SECRET as string,
-//     client_id: PAYPAL_CLIENT_ID as string,
-//     mode: "sandbox"
-// })
-// export async function GET() {
-//     const data = await twillio.sendMessage({ message: "Hi by", number: "+639553389297" })
-//     console.log(data)
-//     return NextResponse.json({})
-// }
+import { NextResponse } from 'next/server'
+import Paypal from '@/services/paypal'
+import Twillio from '@/services/twilio'
+const twillio = new Twillio()
+const { PAYPAL_SECRET, PAYPAL_CLIENT_ID, NEXTAUTH_URL } = process.env
+const paypal = new Paypal({
+    client_secret: PAYPAL_SECRET as string,
+    client_id: PAYPAL_CLIENT_ID as string,
+    mode: "sandbox"
+})
+export async function GET() {
+    // const data = await twillio.sendMessage({ message: "Hi by", number: "+639553389297" })
+    // console.log(data)
+    return NextResponse.json({})
+}
