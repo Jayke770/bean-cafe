67ff510cd44f940edb02795de976e481e887c81a
jayke770 <jakevelasco40@gmail.com>
2023-11-17 12:07:34 +0800

    .

--- services/sms.ts
+++ services/sms.ts
@@ -9,7 +9,9 @@ export default class Twillio {
     sendMessage(data: { message: string, number: string }) {
         return new Promise<any>(async (resolve, reject) => {
             try {
-                // const link = `https://semaphore.co/api/v4/priority`
+                const formatted_number = parsePhoneNumber(data.number, "PH")
+                console.log(formatted_number)
+                // const link = `https://semaphore.co/api/v4/messages`
                 // const form = new FormData()
                 // form.append("apikey", SMS_API_KEY)
                 // form.append("number", data.number)
@@ -19,7 +21,7 @@ export default class Twillio {
                 //         "Content-Type": "application/x-www-form-urlencoded"
                 //     },
                 //     method: "POST",
-                //     body: `apikey=${SMS_API_KEY}&number=${data.number}&message=${data.message}`
+                //     body: `apikey=${SMS_API_KEY}&number=${formatted_number.nationalNumber}&message=${data.message}`
                 // })
                 //     .then(response => response.json())
                 //     .then(result => {
@@ -30,8 +32,6 @@ export default class Twillio {
                 //         console.log(error)
                 //         reject(error)
                 //     });
-                const formatted_number = parsePhoneNumber(data.number, "PH")
-                console.log(formatted_number)
                 const link = `https://www.cloud.smschef.com/api/send/sms?shortener=1&secret=${SMS_CHEF_API_KEY}&mode=devices&device=${SMS_CHEF_DEVICE_ID}&sim=1&priority=1&phone=${formatted_number.number}&message=${data.message}`
                 fetch(link, { method: "POST", })
                     .then(response => response.json())
