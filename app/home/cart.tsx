57f81aeedb49970af2eba84e0e0bf9226d6ecefe
jayke770 <jakevelasco40@gmail.com>
2023-11-17 11:44:07 +0800

    .

--- app/home/cart.tsx
+++ app/home/cart.tsx
@@ -282,9 +282,9 @@ export default function Cart({
                                                 )}
                                                 <ListItem
                                                     link
-                                                    touchRipple={(selectedItemIncart?.items?.reduce((sum, item) => sum + ((item.price * item.quantity) + (item?.addon?.price ?? 0)), 0)) <= 200}
-                                                    className={(selectedItemIncart?.items?.reduce((sum, item) => sum + ((item.price * item.quantity) + (item?.addon?.price ?? 0)), 0)) <= 200 ? '' : 'opacity-20 '}
-                                                    onClick={() => (selectedItemIncart?.items?.reduce((sum, item) => sum + ((item.price * item.quantity) + (item?.addon?.price ?? 0)), 0)) <= 200 && onSetDelivery("deliver")}
+                                                    touchRipple={(selectedItemIncart?.items?.reduce((sum, item) => sum + ((item.price * item.quantity) + (item?.addon?.price ?? 0)), 0)) >= 200}
+                                                    className={(selectedItemIncart?.items?.reduce((sum, item) => sum + ((item.price * item.quantity) + (item?.addon?.price ?? 0)), 0)) >= 200 ? '' : 'opacity-20 '}
+                                                    onClick={() => (selectedItemIncart?.items?.reduce((sum, item) => sum + ((item.price * item.quantity) + (item?.addon?.price ?? 0)), 0)) >= 200 && onSetDelivery("deliver")}
                                                     chevron={false}
                                                     title="Deliver"
                                                     media={
@@ -297,7 +297,7 @@ export default function Cart({
                                             <div className='px-3.5 mt-2'>
                                                 {(selectedItemIncart?.items?.reduce((sum, item) => sum + ((item.price * item.quantity) + (item?.addon?.price ?? 0)), 0)) <= 200 && (
                                                     <div className='border text-center border-amber-600 text-amber-500 rounded p-3'>
-                                                        Cash on Delivery is only available on orders with a total value of ₱200 or more.
+                                                        Cash on Delivery is only available within Maranding only on orders with a total value of ₱200 or more.
                                                     </div>
                                                 )}
                                             </div>
@@ -447,8 +447,8 @@ export default function Cart({
                                         } /> */}
                                     <ListItem
                                         link
-                                        touchRipple={(selectedItemIncart?.items?.reduce((sum, item) => sum + ((item.price * item.quantity) + (item?.addon?.price ?? 0)), 0)) <= 200}
-                                        className={` col-span-full ${(selectedItemIncart?.items?.reduce((sum, item) => sum + ((item.price * item.quantity) + (item?.addon?.price ?? 0)), 0)) <= 200 ? '' : 'opacity-20 '}`}
+                                        touchRipple={(selectedItemIncart?.items?.reduce((sum, item) => sum + ((item.price * item.quantity) + (item?.addon?.price ?? 0)), 0)) >= 200}
+                                        className={` col-span-full ${(selectedItemIncart?.items?.reduce((sum, item) => sum + ((item.price * item.quantity) + (item?.addon?.price ?? 0)), 0)) >= 200 ? '' : 'opacity-20 '}`}
                                         chevron={false}
                                         title="Cash on Delivery"
                                         onClick={() => onSelectPaymentMethod("cash_on_delivery")}
